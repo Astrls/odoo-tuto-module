c69e9e36f34493988e5dd702843b0d0f7791ea00
Antoine Struelens <antoine.struelens@gmail.com>
2023-10-17 15:56:05 +0200

    add UI and defaults for property objects

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,13 +5,13 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate properties"
 
-    name = fields.Char('Property Name', required=True)
+    name = fields.Char('Property Name', default="Unknown", required=True)
     description = fields.Text('Property Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Availability')
+    date_availability = fields.Date('Availability', copy=False, default=lambda self: fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float('Expected Price', required=True)
-    selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('# of bedrooms')
+    selling_price = fields.Float('Selling Price', readonly=True, copy=False)
+    bedrooms = fields.Integer('# of bedrooms', default=2)
     living_area = fields.Integer('Living Area')
     facades = fields.Integer('# facades')
     garage = fields.Boolean('Garage')
@@ -20,5 +20,4 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         string='Garden Orientation',
         selection=[('north','North'),('east','East'),('south','South'),('west','West')])
-
 
